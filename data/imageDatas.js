49b9c4ee64d6c59f59ea1c7f84b164f9aa1f80e3
qq8697 <1058806283@qq.com>
2019-03-21 09:38:06 +0800

    update partial info

--- data/imageDatas.js
+++ data/imageDatas.js
@@ -1,12 +1,42 @@
 var imageDatas=[
 	{
-		"fileName":"7.jpg",
-		"title":"那些年你们一起追过的女孩",
-		"desc":"description7"
+		"fileName":"1.jpg",
+		"title":"益智区",
+		"desc":"投放一些能促进幼儿观察、比较、分析、推理、判断等启发幼儿思考的材料，供幼儿进行操作和游戏。"
 	},
 	{
-		"fileName":"7.jpg",
-		"title":"那些年你们一起追过的女孩",
-		"desc":"description7"
+		"fileName":"2.jpg",
+		"title":"阅读角",
+		"desc":"阅读是孩子成长与终生学习的最重要的能力，作为家长和老师，爱孩子，就让Ta爱上阅读吧。"
+	},
+	{
+		"fileName":"3.jpg",
+		"title":"建构区",
+		"desc":"建构游戏是一种创造性的游戏，是幼儿根据自己的生活经验，以想象为中心，主动地的模仿或创造以反映周围现实生活的游戏"
+	},
+	{
+		"fileName":"4.jpg",
+		"title":"感统训练",
+		"desc":"感统训练是一种在游戏和玩乐的过程中刺激孩子大脑功能，锻炼孩子的前庭觉、触觉和本体感，从而提高孩子感觉统合能力的训练。"
+	},
+	{
+		"fileName":"0.jpg",
+		"title":"睡眠区",
+		"desc":""
+	},
+	{
+		"fileName":"0.jpg",
+		"title":"美工区",
+		"desc":""
+	},
+	{
+		"fileName":"0.jpg",
+		"title":"娃娃区",
+		"desc":""
+	},
+	{
+		"fileName":"0.jpg",
+		"title":"植物角",
+		"desc":""
 	}
 ]
